c8cd9fb52e87ff05f013601107ea56669d1a1175
DJustice96 <damounderscore1019@hotmail.com>
2021-02-19 07:10:35 +1100

    Added functions to clean code. Streamlined code.

--- USADisplay.py
+++ USADisplay.py
@@ -7,16 +7,15 @@ cur = conn.cursor()
 
 
 count = 0
+
 nullcount = 0
 ASCDSC = input("Would you like to see the Highest or Lowest wages? ").lower()
 howmany = input("How many itterations would you like?")
 howmany = int(howmany)
-
-#Organises SQL data in ascending order and prints x number of iterations
-if ASCDSC == "highest":
-    print(ASCDSC)
-
-    if howmany == str(howmany) or howmany >= 531:
+def highest():
+    global count
+    global nullcount
+    if howmany == str(howmany):
         print("ERROR: Please Enter a number between 1-530!")
         quit()
 
@@ -26,29 +25,29 @@ if ASCDSC == "highest":
 #Pulls all the data for the wage2018 column(skipping NULL cells), converts to floating point
 #and prints job and latest wage data
     for row in cur:
-        if row[6] == "" or row[6] == "Average Wage (2018)":
+        if row[6] == 0:
             nullcount += 1
             continue
         count = count + 1
-        wage = float(row[6])
-        wage = round(wage, 2)
+        wage = round(row[6], 2)
         if count > howmany:
             break
         else:
             print("\n",row[1],"-------------", "$", wage)
-    print("\n===Skipped", nullcount,"entries due to insufficient data===")
-#Organises SQL data in descending order and prints x number of iterations
-elif ASCDSC == "Lowest" or ASCDSC == "lowest":
-
-    #if howmany == str(howmany) or howmany >= max(id):
-        #print("ERROR: Please Enter a number between 1-530!")
-    #    quit()
+            print("\n===Skipped", nullcount,"entries due to insufficient data===")
+def lowest():
+    global count
+    global nullcount
+    if howmany == str(howmany):
+        print("ERROR: Please Enter a number between 1-530!")
+        quit()
     cur.execute('SELECT * FROM Data WHERE Wage2018 > 0 ORDER BY Wage2018 ASC')
     print("Bottom", howmany, "wages in the US (As of 2018): \n")
 #Pulls all the data for the wage2018 column(skipping NULL cells), converts to floating point
 #and prints job and latest wage data
+
     for row in cur:
-        if row[6] == "" or row[6] == "Average Wage (2018)":
+        if row[6] == 0:
             nullcount += 1
             continue
         count = count + 1
@@ -58,6 +57,14 @@ elif ASCDSC == "Lowest" or ASCDSC == "lowest":
         else:
             print("\n",row[1],"-------------", "$", wage)
     print("\n===Skipped", nullcount,"entries due to insufficient data===")
+#Organises SQL data in ascending order and prints x number of iterations
+
+if ASCDSC == "highest":
+    highest()
+
+#Organises SQL data in descending order and prints x number of iterations
+elif ASCDSC == "lowest":
+    lowest()
 
 else:
     print("ERROR: Please enter either 'Highest' or 'Lowest'")
@@ -67,16 +74,15 @@ highGrowRate = 0.0
 highGrowJob = None
 count = 0
 
-YN = input('\n\nWould you like to see additional data? Y/N ')
-if YN == "Y" or YN == "y":
+YN = input('\n\nWould you like to see additional data? Y/N ').lower()
+if YN == "y":
     print("\nGrowth Rate of these positions: ")
-    if ASCDSC == "Highest" or ASCDSC == "highest":
+    if ASCDSC == "highest":
         try:
-
             cur.execute('SELECT * FROM Data WHERE Wage2018 > 0 ORDER BY Wage2018 DESC')
-        except Exception as i:
-            print(i)
-    elif ASCDSC == "Lowest" or ASCDSC == "lowest":
+        except:
+            pass
+    elif ASCDSC == "lowest":
         cur.execute('SELECT * FROM Data WHERE Wage2018 > 0 ORDER BY Wage2018 ASC')
 
     else:
@@ -89,8 +95,6 @@ if YN == "Y" or YN == "y":
         else:
             try:
                 median = (((row[3] - row[2]) / row[3]) * 100) + (((row[4] - row[3]) / row[4]) * 100) + (((row[5] - row[4]) / row[5]) * 100) + (((row[6] - row[5]) / row[6]) * 100)
-
-                #=====BUG====== - getting traceback saying row[x] is str but is actually float as show by type(row[x])
 
                 median = round(median, 2)
                 print("\nGrowth of",row[1],"= %",median, "average per year")
