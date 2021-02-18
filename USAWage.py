4bd3d531efe6805cf738135b7803116dc0f87d40
DJustice96 <damounderscore1019@hotmail.com>
2021-02-18 21:06:49 +1100

    Added early float conversion and set NULL cells to 0

--- USAWage.py
+++ USAWage.py
@@ -13,16 +13,20 @@ id = 0
 
 with open('Data USA Cart.csv', newline='') as File:
     reader = csv.reader(File)
+    next(reader, None)
     for column in reader:
+
         position = column[1]
-        wage2014 = column[2]
-        wage2015 = column[3]
-        wage2016 = column[4]
-        wage2017 = column[5]
-        wage2018 = column[6]
+        wage2014 = float(column[2]) if column[2] != "" else 0.0
+        wage2015 = float(column[3]) if column[3] != "" else 0.0
+        wage2016 = float(column[4]) if column[4] != "" else 0.0
+        wage2017 = float(column[5]) if column[5] != "" else 0.0
+        wage2018 = float(column[6]) if column[6] != "" else 0.0
         id = id + 1
         print(position, wage2014, wage2015, wage2016, wage2017, wage2018)
         #print(row)
+
+
 
         cur.execute('''INSERT OR REPLACE INTO Data(ID, position, Wage2014, Wage2015, Wage2016, Wage2017, Wage2018)
         VALUES (?, ?, ?, ?, ?, ?, ?)''', (id, position, wage2014, wage2015, wage2016, wage2017, wage2018))
