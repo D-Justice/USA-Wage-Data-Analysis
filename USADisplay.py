b032da807630bb42e51c9fc7c270219abd173e41
DJustice96 <damounderscore1019@hotmail.com>
2021-02-18 21:05:51 +1100

    Fixed ascention bug

--- USADisplay.py
+++ USADisplay.py
@@ -8,17 +8,19 @@ cur = conn.cursor()
 
 count = 0
 nullcount = 0
-ASCDSC = input("Would you like to see the Highest or Lowest wages? ")
+ASCDSC = input("Would you like to see the Highest or Lowest wages? ").lower()
 howmany = input("How many itterations would you like?")
 howmany = int(howmany)
+
 #Organises SQL data in ascending order and prints x number of iterations
-if ASCDSC == "Highest" or ASCDSC == "highest":
+if ASCDSC == "highest":
+    print(ASCDSC)
 
     if howmany == str(howmany) or howmany >= 531:
         print("ERROR: Please Enter a number between 1-530!")
         quit()
 
-    cur.execute('SELECT * FROM Data ORDER BY Wage2018 DESC')
+    cur.execute('SELECT * FROM Data WHERE Wage2018 > 0 ORDER BY Wage2018 DESC')
 
     print("Top", howmany, "wages in the US (As of 2018): \n")
 #Pulls all the data for the wage2018 column(skipping NULL cells), converts to floating point
@@ -38,10 +40,10 @@ if ASCDSC == "Highest" or ASCDSC == "highest":
 #Organises SQL data in descending order and prints x number of iterations
 elif ASCDSC == "Lowest" or ASCDSC == "lowest":
 
-    if howmany == str(howmany) or howmany >= 531:
-        print("ERROR: Please Enter a number between 1-530!")
-        quit()
-    cur.execute('SELECT * FROM Data ORDER BY Wage2018 ASC')
+    #if howmany == str(howmany) or howmany >= max(id):
+        #print("ERROR: Please Enter a number between 1-530!")
+    #    quit()
+    cur.execute('SELECT * FROM Data WHERE Wage2018 > 0 ORDER BY Wage2018 ASC')
     print("Bottom", howmany, "wages in the US (As of 2018): \n")
 #Pulls all the data for the wage2018 column(skipping NULL cells), converts to floating point
 #and prints job and latest wage data
@@ -50,12 +52,11 @@ elif ASCDSC == "Lowest" or ASCDSC == "lowest":
             nullcount += 1
             continue
         count = count + 1
-        wage = float(row[6])
-        wage = round(wage, 2)
+        wage = round(row[6], 2)
         if count > howmany:
             break
         else:
-            print(row[1],"-------------", "$", wage)
+            print("\n",row[1],"-------------", "$", wage)
     print("\n===Skipped", nullcount,"entries due to insufficient data===")
 
 else:
@@ -71,11 +72,12 @@ if YN == "Y" or YN == "y":
     print("\nGrowth Rate of these positions: ")
     if ASCDSC == "Highest" or ASCDSC == "highest":
         try:
-            cur.execute('SELECT * FROM Data ORDER BY Wage2018 DESC')
+
+            cur.execute('SELECT * FROM Data WHERE Wage2018 > 0 ORDER BY Wage2018 DESC')
         except Exception as i:
             print(i)
     elif ASCDSC == "Lowest" or ASCDSC == "lowest":
-        cur.execute('SELECT * FROM Data ORDER BY Wage2018 ASC')
+        cur.execute('SELECT * FROM Data WHERE Wage2018 > 0 ORDER BY Wage2018 ASC')
 
     else:
         print("ERROR: Please type either Y or N to continue")
@@ -91,9 +93,9 @@ if YN == "Y" or YN == "y":
                 #=====BUG====== - getting traceback saying row[x] is str but is actually float as show by type(row[x])
 
                 median = round(median, 2)
-                print("Growth of",row[1],"= %",median, "average per year")
+                print("\nGrowth of",row[1],"= %",median, "average per year")
             except Exception as i:
-                print("Error finding historical data for:",row[0], row[1])
+                print("\nError finding historical data for:",row[0], row[1])
                 continue
 
             if median > highGrowRate:
